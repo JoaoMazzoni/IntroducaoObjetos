c13b9a251db69aa010cac2067290d7f64c5ba101
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-29 10:48:14 -0300

    Add project files.

---
+++ IntroducaoObjetos/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade; 
+
+        public Pessoa()
+        {
+
+        }
+
+        public string definirNome()
+        {
+            Console.WriteLine("Informe o nome desejado: ");
+            //string Nome;
+            // Nome =  Console.ReadLine();
+            //return Nome;
+            return Console.ReadLine(); //O que a pessoa digitar será retornado. (Mesma coisa que o comentário)
+        }
+
+        public int definirIdade()
+        {
+            Console.WriteLine("Informe a idade: ");
+            //int Idade;
+            // Idade =  int.Parse(Console.ReadLine());
+            //return Idade;
+            return int.Parse(Console.ReadLine());
+        }
+
+    }
+
---
+++ IntroducaoObjetos/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Pessoa pessoa = new Pessoa(); //Instanciando o objeto "pessoa".
+
+pessoa.Nome = pessoa.definirNome(); //Indicando quem vai receber o retorno do método | "pessoa.Nome" vai receber o dado inserido em "pessoa.definirNome"
+pessoa.Idade = pessoa.definirIdade(); //Indicando quem vai receber o retorno do método | "pessoa.Idade" vai receber o dado inserido em "pessoa.definirIdade"
+
+Console.WriteLine(pessoa.Nome); //Imprime os dados salvos no objeto "pessoa" no atributo "Nome".
+Console.WriteLine(pessoa.Idade);
